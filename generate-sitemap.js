167a9a0018e757a2ad74553835cfa39e91780c37
Oprockyvai <141143459+Oprockyvai@users.noreply.github.com>
2025-08-15 19:39:38 +0600

    Update generate-sitemap.js

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -1,43 +1,20 @@
-const fs = require('fs');
-const { videoList } = require('./search-data.js');
+import fs from 'fs';
+import { videoList } from './search-data.js';
 
-const domain = "https://mydeshiflix.pro";
-const lastMod = new Date().toISOString().split('T')[0]; // YYYY-MM-DD
+const baseUrl = 'https://mydeshiflix.pro/video.html?id=';
 
-// Sitemap start
 let sitemap = `<?xml version="1.0" encoding="UTF-8"?>\n`;
 sitemap += `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n`;
 
-// Categories
-const categories = [
-  "milf","new","office","petite","russian",
-  "shemale","squirting","stepmom","stepdad",
-  "stepsis","stepbrother","teen","workout","xxx"
-];
-
-categories.forEach(cat => {
-  sitemap += `  <url>
-    <loc>${domain}/category.html?cat=${cat}</loc>
-    <lastmod>${lastMod}</lastmod>
-    <changefreq>weekly</changefreq>
-  </url>\n`;
-});
-
-// Videos
 videoList.forEach(video => {
-  let videoUrl = video.embed.startsWith('http') 
-    ? video.embed 
-    : `${domain}${video.embed}`;
-  sitemap += `  <url>
-    <loc>${videoUrl}</loc>
-    <lastmod>${lastMod}</lastmod>
-    <changefreq>monthly</changefreq>
-  </url>\n`;
+  sitemap += `  <url>\n`;
+  sitemap += `    <loc>${baseUrl}${video.id}</loc>\n`;
+  sitemap += `    <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>\n`;
+  sitemap += `    <changefreq>monthly</changefreq>\n`;
+  sitemap += `  </url>\n`;
 });
 
-// Close
 sitemap += `</urlset>`;
 
-// Save file
-fs.writeFileSync('sitemap.xml', sitemap);
-console.log("✅ sitemap.xml generated successfully!");
+fs.writeFileSync('sitemap.xml', sitemap, 'utf8');
+console.log('Sitemap generated successfully!');
